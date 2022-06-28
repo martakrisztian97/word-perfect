de75c2d3270f7ff165d5f0a8d0d954f353097275
Márta Krisztián <martakrisztian97@gmail.com>
2022-06-28 19:32:29 +0200

    Create SplashScreen, delete /view/.gitkeep and modified .gitignore

---
+++ src/view/LoadingApp.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+/**
+ * SplashScreen képernyő betöltését szolgáló osztály
+ *
+ * @author Márta Krisztián
+ * @since 2022-06-28
+ * @version 1.0
+ */
+public class LoadingApp {
+
+    public static void main(String[] args) {
+        SplashScreen s = new SplashScreen();
+        s.setVisible(true);
+        try {
+            for (int i = 0; i <= 100; i++) {
+                Thread.sleep(40);
+                s.jLabel3.setText(Integer.toString(i)+"%");
+                if (i == 100) {
+                    s.dispose();
+                }
+            }
+        } catch (Exception e) {
+        }
+    }
+}
