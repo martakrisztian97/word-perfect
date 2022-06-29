c93dbb603f2a9de076fe81f761ef486ea9a1d4b9
Márta Krisztián <martakrisztian97@gmail.com>
2022-06-29 18:41:43 +0200

    Create Home screen

--- src/view/LoadingApp.java
+++ src/view/LoadingApp.java
@@ -15,12 +15,16 @@ public class LoadingApp {
     public static void main(String[] args) {
         SplashScreen s = new SplashScreen();
         s.setVisible(true);
+        Home h = new Home();
         try {
             for (int i = 0; i <= 100; i++) {
                 Thread.sleep(40);
                 s.percentLabel.setText(Integer.toString(i)+"%");
                 if (i == 50) s.loadTextLabel.setText("Magyar szavak betöltése...");
-                if (i == 100) s.dispose();
+                if (i == 100) {
+                    s.dispose();
+                    h.show();
+                }
             }
         } catch (Exception e) {
             System.out.println("Hiba a splash screen betöltésekor!");
